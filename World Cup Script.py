fb9ad003408e24e6637439c09a1f4c66ddcdae2e
HappyStick <34913915+HappyStick@users.noreply.github.com>
2018-03-07 14:07:09 -0800

    Added Match Time Functionality
    Removed the old system of picking match number which then pulled a prepared match.png with the match time.
    
    Implemented drop downs to pick Month Day Time manually so that there's no photoshop prep required. This includes back-end implementation of a .html file that allows for CSS to be used on OBS .txt files so that we can add character spacing.

--- World Cup Script.py
+++ World Cup Script.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import shutil
+import math
 
 scriptsPath = 'F:/World Cup/2018 - Mania 7K/Stream/Scripts/'
 flagsPath = 'F:/World Cup/2018 - Mania 7K/Stream/Scripts/Flags/'
@@ -9,13 +10,9 @@ applicationsPath = 'F:/World Cup/2018 - Mania 7K/Stream/Scripts/Applications/'
 
 def countryName(color, country):
 
-    # Creates the country.txt files
+    # Creates the country.txt files, writes user input into the .txt and closes it
     countryFile = open((f'{scriptsPath}country_file_{color}.txt'), 'w')
-
-    # Writes the user input into the above .txt
     countryFile.write(country)
-
-    # Closes the file
     countryFile.close()
 
 def flagLocation(color, country):
@@ -51,12 +48,11 @@ def playerLists(color, country):
 
     playerList.close()
 
-def runList(match):
+def matchTimes(month, day, time):
 
-    # Sets source and destination vars for selected match and copies it over
-    sourceMatch = (f'{matchesPath}{match}.png')
-    destinationMatch = (f'{scriptsPath}current_match.png')
-    shutil.copyfile(sourceMatch, destinationMatch)
+    matchFile = open((f'{scriptsPath}matchTime.txt'), 'w')
+    matchFile.write((f'{day} {month} {time} UTC'))
+    matchFile.close()
 
 # custom function to allow multiple functions in the button
 def multiBtn():
@@ -70,7 +66,7 @@ def multiBtn():
     playerLists('red', selectedCountry.get())
     playerLists('blue', selectedCountry2.get())
 
-    runList(selectedMatch.get())
+    matchTimes(selectedMonth.get(), str(selectedDay.get()), selectedTime.get())
 
 root = tk.Tk()
 
@@ -85,11 +81,14 @@ teamLabelRed.pack(side=tk.LEFT)
 teamLabelBlue = tk.Label(root, text='Team Blue')
 teamLabelBlue.pack(side=tk.RIGHT)
 
-matchLabel = tk.Label(root, text='Match Number')
-matchLabel.pack(side=tk.TOP)
-
 countryList = ['Argentina', 'Australia', 'Brazil', 'Canada', 'Chile', 'China', 'France', 'Germany', 'Indonesia', 'Italy', 'Japan', 'Malaysia', 'Philippines', 'Singapore', 'South Korea', 'United States']
-matchList = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+dayList = range(1, 32)
+timeList = []
+for number in range(0, 48):
+    hours = math.floor(number/2)
+    mins = number % 2
+    timeList.append(f'{hours:02}:{mins*30:02}')
 
 # Creates the var and initial selected variable in the list we're creating
 selectedCountry = tk.StringVar()
@@ -98,8 +97,14 @@ selectedCountry.set(countryList[0])
 selectedCountry2 = tk.StringVar()
 selectedCountry2.set(countryList[0])
 
-selectedMatch = tk.StringVar()
-selectedMatch.set(matchList[0])
+selectedMonth = tk.StringVar()
+selectedMonth.set(monthList[0])
+
+selectedDay = tk.StringVar()
+selectedDay.set(dayList[0])
+
+selectedTime = tk.StringVar()
+selectedTime.set(timeList[0])
 
 # Creates an optionmenu in below vars with root, selection and list
 countryMenu = tk.OptionMenu(root, selectedCountry, *countryList)
@@ -108,11 +113,17 @@ countryMenu.pack(side=tk.LEFT)
 countryMenu2 = tk.OptionMenu(root, selectedCountry2, *countryList)
 countryMenu2.pack(side=tk.RIGHT)
 
-matchMenu = tk.OptionMenu(root, selectedMatch, *matchList)
-matchMenu.pack(side=tk.TOP)
+monthMenu = tk.OptionMenu(root, selectedMonth, *monthList)
+monthMenu.pack(side=tk.TOP)
+
+dayMenu = tk.OptionMenu(root, selectedDay, *dayList)
+dayMenu.pack(side=tk.TOP)
+
+timeMenu = tk.OptionMenu(root, selectedTime, *timeList)
+timeMenu.pack(side=tk.TOP)
 
 # Create the button that runs most code
 runBtn = tk.Button(root, text="Run", command=multiBtn)
-runBtn.pack(side=tk.TOP)
+runBtn.pack(side=tk.BOTTOM)
 
 root.mainloop()
