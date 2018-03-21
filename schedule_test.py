042544a6ab0df44732b6aec940fb45085779c0af
HappyStick <34913915+HappyStick@users.noreply.github.com>
2018-03-21 00:44:12 -0700

    Websocket addition
    Allows user to check box for elements they want on screen
    
    Outstanding:
    1. Cleanup.......
    2. Implementation of save file loading

--- schedule_test.py
+++ schedule_test.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 import json
+import asyncio
 
 from shutil import copyfile
+from obswsrc import OBSWS
+from obswsrc.requests import ResponseStatus, SetSourceRenderRequest
 
 flags_path = 'F:/World Cup/2018 - Taiko/Scripts/Flags/'
 schedule_output_path = 'F:/World Cup/2018 - Taiko/Scripts/OBS Output/Schedule Files/'
@@ -10,12 +13,13 @@ script_path = 'F:/World Cup/2018 - Taiko/Scripts/'
 # Each match class instance is assigned a match number, team names and time
 class match:
 
-    def __init__(self, number, team_1, team_2, match_time):
+    def __init__(self, number, team_1, team_2, match_time, done):
 
         self.number = number
         self.team_1 = team_1
         self.team_2 = team_2
         self.match_time = match_time
+        self.done = done
 
 # Copies over the flags and outputs time into .txt
 def schedule(matches):
@@ -25,6 +29,48 @@ def schedule(matches):
 
     with open(f'{schedule_output_path}{matches.number}_time.txt', 'w') as match_time_text:
         match_time_text.write(f'{matches.match_time} UTC')
+
+    # If a tickbox is ticked it hides all associated elements in OBS
+    def hide():
+
+        async def hide_2():
+
+            await asyncio.sleep(0.01)
+            async with OBSWS('localhost', 4444, password) as obsws:
+                await asyncio.wait( [
+                    obsws.require(SetSourceRenderRequest(source=f'flag_{matches.number}', render=False, scene_name='Scene')),
+                    obsws.require(SetSourceRenderRequest(source=f'flag_{matches.number}_{matches.number}', render=False, scene_name='Scene')),   
+                    obsws.require(SetSourceRenderRequest(source=f'text_{matches.number}', render=False, scene_name='Scene'))
+                ] )
+        
+        hide_2()
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(hide_2())
+
+    # If a tickbox is unticked it unhides all associated elements in OBS
+    def unhide():
+
+        async def unhide_2():
+
+            await asyncio.sleep(0.01)
+            async with OBSWS('localhost', 4444, password) as obsws:
+                await asyncio.wait( [
+                    obsws.require(SetSourceRenderRequest(source=f'flag_{matches.number}', render=True, scene_name='Scene')),
+                    obsws.require(SetSourceRenderRequest(source=f'flag_{matches.number}_{matches.number}', render=True, scene_name='Scene')),   
+                    obsws.require(SetSourceRenderRequest(source=f'text_{matches.number}', render=True, scene_name='Scene'))
+                ] )
+        
+        unhide_2()
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(unhide_2())
+
+    # Checks if checkbox is ticked and runs appropriate function
+    if matches.done == 1:
+        hide()
+    elif matches.done == 0:
+        unhide()
 
 # Creates var count instances of frames with Match #, Team 1 / 2 and time
 def frames(count):
@@ -42,8 +88,12 @@ def frames(count):
     country_menu.pack(side = tk.LEFT)
 
     time_box = tk.Entry(frame, textvariable = time_input_list[count], width = '5')
-    time_box.pack(side = tk.RIGHT)
+    time_box.pack(side = tk.LEFT)
 
+    check_box_box = tk.Checkbutton(frame, variable = check_box_list[count])
+    check_box_box.pack(side = tk.RIGHT) 
+
+# Creates variables for all data that might need saving and outputs it in a .json file
 def save_button():
 
     frame_count = 0
@@ -51,6 +101,7 @@ def save_button():
     country_1 = []
     country_2 = []
     time_1 = []
+    check_box_1 = []
 
     for count in range(0, match_amount):
 
@@ -59,8 +110,9 @@ def save_button():
         country_1.append(selected_country_list[count * 2].get())
         country_2.append(selected_country_list[count * 2 + 1].get())
         time_1.append(time_input_list[count].get())
+        check_box_1.append(check_box_list[count].get())
 
-    saved_vars = {"saved_vars" : [{"team_1" : country_1}, {"team_2" : country_2}, {"time" : time_1}]}
+    saved_vars = {"saved_vars" : [{"team_1" : country_1}, {"team_2" : country_2}, {"time" : time_1}, {"checkbox" : check_box_1}]}
     
     with open(f'{script_path}savefile.json', 'w') as file:
         json.dump(saved_vars, file, indent = 4)
@@ -74,7 +126,7 @@ def run_button():
 
         frame_count = frame_count + 1
 
-        matches = match(f'{frame_count}', selected_country_list[count * 2].get(), selected_country_list[count * 2 + 1].get(), time_input_list[count].get())
+        matches = match(f'{frame_count}', selected_country_list[count * 2].get(), selected_country_list[count * 2 + 1].get(), time_input_list[count].get(), check_box_list[count].get())
 
         schedule(matches)
 
@@ -83,17 +135,22 @@ root = tk.Tk()
 root.title('OBS Schedule Tool')
 root.geometry('500x500')
 
-#creates country_list based on schedule.json
+# Creates country_list based on schedule.json
 with open(f'{script_path}countries.json') as file:
     country_data = json.load(file)
 
 country_list = []
 for countries in country_data['countries']:
     country_list.append(countries['abbreviation'])
+
 time_list = ['00:00']
 
+check_box_input = [False]
+
+# Creates empty country, time and checkbox lists
 selected_country_list = []
 time_input_list = []
+check_box_list = []
 
 match_amount = int(input('Match count?:\n'))
 
@@ -106,6 +163,13 @@ for numbers in range(0, match_amount):
     time_input = tk.StringVar()
     time_input.set(time_list[0])
     time_input_list.append(time_input)
+
+# Creates 12 iterations of checkbox to be used by frames() at initial runtime
+for numbers in range(0, match_amount):
+
+    check_box = tk.IntVar()
+    check_box.set(check_box_input[0])
+    check_box_list.append(check_box)
 
 # Creates 24 iterations of selected_country to be used by frames() at initial runtime
 for numbers in range(0, match_amount * 2):
