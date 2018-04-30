6f19b628eaeeb4309963fc75788f795822e200d6
HappyStick <34913915+HappyStick@users.noreply.github.com>
2018-04-29 20:52:52 -0700

    Added functionality & Improvements
    - Added "Reset Schedule" functionality to clear the entire scene
    - Added "Next Match" which goes through a couple of essentials when moving on to the next match such as resetting the mappool
    - Added "Conditional" functionality where if a match is LB & conditional it shows it along with an explanation of the concept

--- schedule.py
+++ schedule.py
@@ -11,17 +11,19 @@ schedule_output_path = "F:/World Cup/2018 - Taiko/Scripts/OBS Output/Schedule Fi
 current_output_path = "F:/World Cup/2018 - Taiko/Scripts/OBS Output/Current Files/"
 script_path = "F:/World Cup/2018 - Taiko/Scripts/"
 resources_path = "F:/World Cup/2018 - Taiko/Scripts/Resources/"
+maps_path = "F:/World Cup/2018 - Taiko/Scripts/Resources/Maps/"
 
 # Each match class instance is assigned a match number, team names and time
 class match:
 
-    def __init__(self, number, team_1, team_2, match_time, done, match_score_1, match_score_2):
+    def __init__(self, number, team_1, team_2, match_time, done, conditional, match_score_1, match_score_2):
 
         self.number = number
         self.team_1 = team_1
         self.team_2 = team_2
         self.match_time = match_time
         self.done = done
+        self.conditional = conditional
         self.match_score_1 = match_score_1
         self.match_score_2 = match_score_2
 
@@ -116,7 +118,14 @@ def schedule(matches):
                     obsws.require(SetSourceRenderRequest(source=f"{matches.number}_vs_done", render=False, scene_name="Schedule")),
 
                     obsws.require(SetSourceRenderRequest(source=f"{matches.number}_hl", render=False, scene_name="Schedule")),
-                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_{matches.number}_hl", render=False, scene_name="Schedule"))
+                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_{matches.number}_hl", render=False, scene_name="Schedule")),
+
+                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_name 2", render=True, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_flag 2", render=True, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_vs 2", render=True, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_{matches.number}_flag 2", render=True, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_{matches.number}_name 2", render=True, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_text 2", render=True, scene_name="Schedule"))
                 ] )
         
         unhide_2()
@@ -124,11 +133,50 @@ def schedule(matches):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(unhide_2())
 
+    #If a match is conditional it will unhide the conditional text element
+    def unhide_conditional():
+
+        async def unhide_conditional_2():
+
+            await asyncio.sleep(0.01)
+            async with OBSWS("localhost", 4444, "PIqDeb0q7QYCNunQUMyC") as obsws:
+                await asyncio.wait( [
+                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_conditional", render=True, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"conditional_explanation", render=True, scene_name="Schedule"))
+                ] )
+        
+        unhide_conditional_2()
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(unhide_conditional_2())
+
+    #If a match is conditional it will hide the conditional text element
+    def hide_conditional():
+
+        async def hide_conditional_2():
+
+            await asyncio.sleep(0.01)
+            async with OBSWS("localhost", 4444, "PIqDeb0q7QYCNunQUMyC") as obsws:
+                await asyncio.wait( [
+                    obsws.require(SetSourceRenderRequest(source=f"{matches.number}_conditional", render=False, scene_name="Schedule"))
+                ] )
+        
+        hide_conditional_2()
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(hide_conditional_2())
+
+
     # Checks if checkbox is ticked and runs appropriate function
     if matches.done == 1:
         hide()
     elif matches.done == 0:
         unhide()
+    
+    if matches.conditional == 1:
+        unhide_conditional()
+    elif matches.conditional == 0:
+        hide_conditional()
 
 # Creates var count instances of frames with Match #, Team 1 / 2 and time
 def frames(count):
@@ -154,8 +202,88 @@ def frames(count):
     score_box_2 = tk.Entry(frame, textvariable = score_input_list_2[count], width = "2")
     score_box_2.pack(side = tk.LEFT)
 
+    check_box_conditional_box = tk.Checkbutton(frame, text = "Cond.", variable = check_box_conditional_list[count])
+    check_box_conditional_box.pack(side = tk.RIGHT)
+
     check_box_box = tk.Checkbutton(frame, text = "Match Over", variable = check_box_list[count])
     check_box_box.pack(side = tk.RIGHT)
+
+# Resets the entire schedule screen to blank. Previously the elements previously used when moving to a smaller schedule would have to be hidden manually.
+def reset_button():
+
+    for count in range(1, 9):
+
+        async def unhide_reset():
+
+            await asyncio.sleep(0.01)
+            async with OBSWS("localhost", 4444, "PIqDeb0q7QYCNunQUMyC") as obsws:
+                await asyncio.wait( [
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_flag", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_{count}_flag", render=False, scene_name="Schedule")), 
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_flag_done", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_{count}_flag_done", render=False, scene_name="Schedule")),
+
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_text", render=False, scene_name="Schedule")),
+
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_name", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_{count}_name", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_name_done", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_{count}_name_done", render=False, scene_name="Schedule")),
+
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_score", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_{count}_score", render=False, scene_name="Schedule")),
+
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_vs", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_vs_done", render=False, scene_name="Schedule")),
+
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_hl", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_{count}_hl", render=False, scene_name="Schedule")),
+
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_text 2", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_{count}_name 2", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_{count}_flag 2", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_vs 2", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_flag 2", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_name 2", render=False, scene_name="Schedule")),
+
+                    obsws.require(SetSourceRenderRequest(source=f"{count}_conditional", render=False, scene_name="Schedule")),
+                    obsws.require(SetSourceRenderRequest(source=f"conditional_explanation", render=False, scene_name="Schedule"))
+                ] )
+        
+        unhide_reset()
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(unhide_reset())
+
+# Runs several actions that need to take place when moving into a new match, avoiding slip-ups
+def next_match_button():
+
+    copyfile(f"{maps_path}transparant.png", f"{current_output_path}banned_map_1.png")
+    copyfile(f"{maps_path}transparant.png", f"{current_output_path}banned_map_2.png")
+    with open(f"{current_output_path}roll_1.txt", "w") as roll_1:
+        roll_1.write("")
+    with open(f"{current_output_path}roll_2.txt", "w") as roll_2:
+        roll_2.write("")
+
+    for count in range(0,15):
+        copyfile(f"{maps_path}{count}_default.png", f"{current_output_path}{count}_map.png")
+
+    def next_match_hide():
+
+        async def next_match_hide_2():
+
+            await asyncio.sleep(0.01)
+            async with OBSWS("localhost", 4444, "PIqDeb0q7QYCNunQUMyC") as obsws:
+                await asyncio.wait( [
+                    obsws.require(SetSourceRenderRequest(source=f"Tourney Client", render=False, scene_name="Mappool")),
+                ] )
+        
+        next_match_hide_2()
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(next_match_hide_2())    
+    
+    next_match_hide()
 
 # Runs code after new inputs are filled in
 def run_button():
@@ -166,7 +294,7 @@ def run_button():
 
         frame_count = frame_count + 1
 
-        matches = match(f"{frame_count}", selected_country_list[count * 2].get(), selected_country_list[count * 2 + 1].get(), time_input_list[count].get(), check_box_list[count].get(), score_input_list_1[count].get(), score_input_list_2[count].get())
+        matches = match(f"{frame_count}", selected_country_list[count * 2].get(), selected_country_list[count * 2 + 1].get(), time_input_list[count].get(), check_box_list[count].get(), check_box_conditional_list[count].get(), score_input_list_1[count].get(), score_input_list_2[count].get())
 
         schedule(matches)
 
@@ -185,6 +313,7 @@ for countries in country_data["countries"]:
 
 time_list = ["00:00"]
 check_box_input = [False]
+check_box_conditional_input = [False]
 score_list_1 = ["0"]
 score_list_2 = ["0"]
 
@@ -192,19 +321,20 @@ score_list_2 = ["0"]
 selected_country_list = []
 time_input_list = []
 check_box_list = []
+check_box_conditional_list = []
 score_input_list_1 = []
 score_input_list_2 = []
 
 match_amount = int(input("Match count?:\n"))
 
-# Creates 12 iterations of time_input to be used by frames() at initial runtime
+# Creates iterations of time_input to be used by frames() at initial runtime
 for numbers in range(0, match_amount):
 
     time_input = tk.StringVar()
     time_input.set(time_list[0])
     time_input_list.append(time_input)
 
-# Creates 12 iterations of score_input to be used by frames() at initial runtime
+# Creates iterations of score_input to be used by frames() at initial runtime
 for numbers in range(0, match_amount):
 
     score_input_1 = tk.StringVar()
@@ -217,14 +347,20 @@ for numbers in range(0, match_amount):
     score_input_2.set(score_list_2[0])
     score_input_list_2.append(score_input_2)
 
-# Creates 12 iterations of checkbox to be used by frames() at initial runtime
+# Creates iterations of checkbox to be used by frames() at initial runtime
 for numbers in range(0, match_amount):
 
     check_box = tk.IntVar()
     check_box.set(check_box_input[0])
     check_box_list.append(check_box)
 
-# Creates 24 iterations of selected_country to be used by frames() at initial runtime
+for numbers in range(0, match_amount):
+
+    check_box_conditional = tk.IntVar()
+    check_box_conditional.set(check_box_conditional_input[0])
+    check_box_conditional_list.append(check_box_conditional)
+
+# Creates iterations of selected_country to be used by frames() at initial runtime
 for numbers in range(0, match_amount * 2):
 
     selected_country = tk.StringVar()
@@ -241,6 +377,12 @@ for count in range(0, match_amount):
 
 run_btn = tk.Button(root, text = "Run", command = run_button)
 run_btn.pack(side = tk.BOTTOM)
+
+reset_btn = tk.Button(root, text = "Reset Schedule", command = reset_button)
+reset_btn.pack(side = tk.BOTTOM)
+
+next_match_btn = tk.Button(root, text = "Next Match", command = next_match_button)
+next_match_btn.pack(side = tk.BOTTOM)
 
 root.mainloop()
 
